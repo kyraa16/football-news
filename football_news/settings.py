a11203cedb0926709ad32cf7643784e3d3564511
kyraa16 <kadekchandra16@gmail.com>
2025-09-10 14:23:58 +0700

    Complete tutorial 2: form and data delivery

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kadek-chandra-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://kadek-chandra-footballnews.pbp.cs.ui.ac.id/"
+    'https://kadek-chandra-footballnews.pbp.cs.ui.ac.id',
 ]
 
 # Application definition
