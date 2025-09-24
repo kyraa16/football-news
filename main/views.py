48787de5c61e6f9bfd2e4e078d3ab084d4e8c165
kyraa16 <kadekchandra16@gmail.com>
2025-09-24 13:30:20 +0700

    Complete tutorial 4: Web Design using HTM & CSS3 and Update & Delete News

--- main/views.py
+++ main/views.py
@@ -23,14 +23,13 @@ def show_main(request):
         news_list = News.objects.filter(user=request.user)
 
     context = {
-        'npm' : '2406426473',
-        'name': 'Kadek Chandra Rasmi',
+        'npm': '2406426473',
+        'name': request.user.username,
         'class': 'PBP E',
         'news_list': news_list,
-        'last_login': request.COOKIES.get('last_login', 'Never')   # mengakses cookie yang terdaftar di request
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
-
-    return render(request, "main.html", context)
+    return render(request, "main.html",context)
 
 def create_news(request):
     form = NewsForm(request.POST or None)
@@ -118,5 +117,23 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')  #menghapus cookie last_login
     return response
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
 
 
